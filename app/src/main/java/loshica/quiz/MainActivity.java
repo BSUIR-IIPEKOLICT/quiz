61fbe82848813ec3aa3b2d166b59b142b65e5d13
Zhenek <54858818+IIPEKOLICT@users.noreply.github.com>
2021-07-23 22:11:52 +0300

    fix: change questions to adequate
    Initial commit
    
    feat: add viewpager and recycleview
    
    fix: more correct and smooth realisation
    
    feat: more correct theme engine and clenup code
    
    feat: migrate to viewpager2 & fix logic
    
    feat: more correct logic & sort leaderboard
    
    feat: add help feature & timer
    
    feat: impromevments of theme engine & new settings activity
    
    feat: add view pager & tabs to main activity, refactor old res
    
    feat: more clear code & new accent picker
    
    feat: fix many bugs & added 9 questions
    
    feat: finally fixed all bugs of logic & added autochange of question activity title
    
    refactor: separated the theme engine and my own things into a separate library
    
    fix: more clear updating activity theme
    
    feat: some impromevments
    
    feat: add mongodb support
    
    feat: migrate to MVC pattern

---
+++ app/src/main/java/loshica/quiz/MainActivity.java
@@ -0,0 +1,100 @@
+package loshica.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.Objects;
+
+import loshica.quiz.controller.Coordinator;
+import loshica.quiz.view.MainAdapter;
+import loshica.quiz.view.MyPageTransformer;
+import loshica.quiz.view.NameDialog;
+import loshica.vendor.LOSSettingsActivity;
+import loshica.vendor.LOSTheme;
+
+public class MainActivity extends AppCompatActivity implements
+    NameDialog.NameDialogListener {
+
+    int theme;
+
+    ViewPager2 mp;
+    MainAdapter ma;
+    TabLayout tab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // LOSTheme
+        theme = new LOSTheme(this).current;
+        setTheme(theme);
+        //
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Main pager
+        mp = findViewById(R.id.main_pager);
+        ma = new MainAdapter(this);
+        mp.setAdapter(ma);
+        mp.setCurrentItem(0);
+        mp.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Objects.requireNonNull(getSupportActionBar()).setTitle(
+                    getResources().getStringArray(R.array.main_tabs)[position]
+                );
+            }
+        });
+        mp.setPageTransformer(new MyPageTransformer());
+        //
+
+        // TabLayout
+        tab = findViewById(R.id.main_tab);
+        new TabLayoutMediator(tab, mp, (tab, position) ->
+            tab.setText(getResources().getStringArray(R.array.main_tabs)[position])
+        ).attach();
+        //
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // If theme changed -> apply new theme
+        if (theme != new LOSTheme(this).current) recreate();
+        //
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.los_menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            startActivity(new Intent(this, LOSSettingsActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void name(String playerName) {
+        Coordinator.startGame(playerName);
+        Coordinator.updateMaps();
+        startActivity(new Intent(this, QuestionActivity.class));
+    }
+
+    public void onBackPressed() {
+        if ((mp.getCurrentItem() > 0)) mp.setCurrentItem(0, true);
+        else finish();
+    }
+}
