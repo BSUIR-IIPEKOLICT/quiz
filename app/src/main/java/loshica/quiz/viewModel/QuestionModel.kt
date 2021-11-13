6c20a4dc025b73f024fccf8c953aa0f944b62324
iipekolict <iipekolict@gmail.com>
2021-11-13 18:20:21 +0300

    feat: change questions

--- app/src/main/java/loshica/quiz/viewModel/QuestionModel.kt
+++ app/src/main/java/loshica/quiz/viewModel/QuestionModel.kt
@@ -14,16 +14,16 @@ class QuestionModel(private val app: Application) : AndroidViewModel(app) {
     init {
         Question.counter = 0
         data.value = arrayOf(
-            Question(R.drawable.shava, R.array.q1),
-            Question(R.drawable.war_and_peace, R.array.q2),
-            Question(R.drawable.paris, R.array.q3),
-            Question(R.drawable.genii, R.array.q4),
-            Question(R.drawable.capuchino, R.array.q5),
-            Question(R.drawable.ice_cream, R.array.q6),
-            Question(R.drawable.borshc, R.array.q7),
-            Question(R.drawable.java, R.array.q8),
-            Question(R.drawable.math, R.array.q9),
-            Question(R.drawable.mountains, R.array.q10)
+            Question(R.drawable.kotlin, R.array.q1),
+            Question(R.drawable.java, R.array.q2),
+            Question(R.drawable.ts, R.array.q3),
+            Question(R.drawable.kotlin, R.array.q4),
+            Question(R.drawable.architecture, R.array.q5),
+            Question(R.drawable.angular, R.array.q6),
+            Question(R.drawable.nest, R.array.q7),
+            Question(R.drawable.question, R.array.q8),
+            Question(R.drawable.question, R.array.q9),
+            Question(R.drawable.architecture, R.array.q10)
         )
         counter.value = 0
     }
